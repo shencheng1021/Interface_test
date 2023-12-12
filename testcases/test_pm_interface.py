3ee858e4276997757d7c8df779f5f29b3eb2756f
shencheng1021 <565072153@qq.com>
2023-12-12 15:47:13 +0800

    增加精准营销用户邀请接口测试

---
+++ testcases/test_pm_interface.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: shencheng
+@software: PyCharm
+@description: 精准营销客户邀请接口测试
+@time: 2022/4/13 9:34
+"""
+import json
+
+import allure
+import pytest
+
+from common.assert_util import AssertUtil
+from common.excel_util import ExcelUtil
+from common.requests_util import RequestsUtil
+from common.yaml_util import YamlUtil
+
+@allure.feature('精准营销接口测试')
+@pytest.mark.usefixtures('pm_initialization')
+class TestPm:
+
+    @allure.step('精准营销客户邀请接口测试')
+    @pytest.mark.parametrize('casedata',ExcelUtil().excel_read('pm_phone'))
+    def test_saveInviteRecords(self,casedata):
+        RequestsUtil().send_code(str(casedata[1]))
+        caseinfo=YamlUtil().read_testcase_yaml('pm_saveInviteRecords.yml')[0]
+        url=caseinfo['requests']['url']
+        method=caseinfo['requests']['method']
+        headers=caseinfo['requests']['headers']
+        caseinfo['requests']['data']['phone']=str(casedata[1])
+        data=json.dumps(caseinfo['requests']['data'])
+        rep=RequestsUtil().send_request(method,url,headers=headers,data=data)
+        result = json.loads(rep)
+        AssertUtil().assertEqual(casedata[2],result['msg'])
+
+
+
+if __name__ == '__main__':
+    pytest.main()
